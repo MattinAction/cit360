18253f34129d5b1a909c8ee057e20a8bf4f2b14a
Matthew Gregory <gregory6267@gmail.com>
2016-10-06 19:52:46 -0600

    Notes from meeting

--- src/Map.java
+++ src/Map.java
@@ -6,4 +6,19 @@ public class Map {
     // NP get/remove something that isn't there
     // NP giving key without a value
     // NP null key
+
+    /*
+    Hash Map Notes
+    Methods
+       .put(key, value)
+       .get(key)
+       .size(Object)
+       .clear(Object)
+       .containsValue(value)
+       .isEmpty(Object)
+       .remove(key)
+
+     Creating
+       HashMap<key datatype, value datatype> myMap = new HashMap<>();
+     */
 }
--- src/Tree.java
+++ src/Tree.java
@@ -2,4 +2,5 @@
  * Created by MattinAction on 9/30/16.
  */
 public class Tree {
+
 }
